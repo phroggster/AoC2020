aaae44a9d39a75fafbf43c5b542d7f0817cfee05
phroggster <phroggster@users.noreply.github.com>
2021-02-03 23:23:22 -0600

    Eight point two!
    - This solution could still be tweaked significantly, but it works.
    - Exposed some problems with my "testing" methodology. Eww.

---
+++ Day08/Computer.cs
@@ -0,0 +1,167 @@
+﻿/*
+ * Solutions to the Advent Of Code 2020
+ * Copyright © 2021 Leland Roach
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Day08
+{
+    public enum OpCode
+    {
+        invalid = 0,
+        acc,
+        jmp,
+        nop,
+    };
+
+    public class State
+    {
+        /// <summary>Accumulator.</summary>
+        public int Eax { get; set; }
+        /// <summary>Instruction pointer, or the index of the instruction that will execute next.</summary>
+        public int Eip { get; set; }
+        /// <summary>List of instruction indices that have already been executed.</summary>
+        public List<int> Esp { get; set; }
+
+        public State(int rax = 0, int rip = 0, List<int> rsp = null)
+        {
+            Eax = rax;
+            Eip = rip;
+            Esp = rsp ?? new List<int>();
+        }
+
+        public void Reset(int rax = 0, int rip = 0, List<int> rsp = null)
+        {
+            Eax = rax;
+            Eip = rip;
+            Esp = rsp ?? new List<int>();
+        }
+    }
+
+    public class Instruction
+    {
+        /// <summary>The argument or data for the instruction.</summary>
+        public int Argument { get; set; }
+        /// <summary>The operation that the instruction will perform.</summary>
+        public OpCode Operation { get; set; }
+
+        public bool CanFlip => Operation == OpCode.jmp || Operation == OpCode.nop;
+
+        public Instruction(in Instruction instruction)
+        {
+            Argument = instruction.Argument;
+            Operation = instruction.Operation;
+        }
+
+        public Instruction(in string instruction)
+        {
+            Argument = int.Parse(instruction[4..]);
+            Operation = Enum.Parse<OpCode>(instruction[0..3]);
+        }
+
+        public bool Flip()
+        {
+            switch (Operation)
+            {
+                case OpCode.jmp:
+                    Operation = OpCode.nop;
+                    return true;
+
+                case OpCode.nop:
+                    Operation = OpCode.jmp;
+                    return true;
+
+                case OpCode.acc:
+                case OpCode.invalid:
+                    return false;
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static List<Instruction> Parse(in List<string> instructions)
+        {
+            var result = new List<Instruction>(instructions.Count);
+            foreach(var inst in instructions)
+            {
+                result.Add(new Instruction(inst));
+            }
+            return result;
+        }
+    };
+
+    public class Computer
+    {
+        public State State { get; set; }
+
+        /// <summary>The program being executed.</summary>
+        public List<Instruction> Program { get; set; }
+
+
+        public Computer(List<Instruction> program = null, State state = null)
+        {
+            Program = program ?? new List<Instruction>();
+            State = state ?? new State();
+        }
+
+        public Computer(List<string> program = null, State state = null)
+        {
+            Program = new List<Instruction>();
+            State = state ?? new State();
+
+            if (program != null && program.Any())
+            {
+                foreach (var inst in program)
+                {
+                    Program.Add(new Instruction(inst));
+                }
+            }
+        }
+
+
+        /// <returns><c>true</c> if the program ran to completion; <c>false</c> if it terminated early.</returns>
+        public bool Run()
+        {
+            while (!State.Esp.Contains(State.Eip) && State.Eip < Program.Count)
+            {
+                Instruction inst = Program[State.Eip];
+                State.Esp.Add(State.Eip);
+
+                switch (inst.Operation)
+                {
+                    case OpCode.acc:
+                        State.Eax += inst.Argument;
+                        break;
+                    case OpCode.jmp:
+                        State.Eip += inst.Argument;
+                        continue;
+                    case OpCode.nop:
+                        break;
+                    default:
+                        throw new ApplicationException($"Invalid OpCode in Program at instruction {State.Eip}: {inst}");
+                }
+                State.Eip++;
+            }
+
+            return State.Eip == Program.Count;
+        }
+    }
+}
--- Day08/Program08.cs
+++ Day08/Program08.cs
@@ -17,6 +17,7 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Day08
@@ -25,144 +26,214 @@ namespace Day08
     {
         static void Main(string[] args)
         {
-            Part1.Exec();
+            Part1();
+            Part2();
         }
-    }
 
-    enum OpCode
-    {
-        invalid = 0,
-        nop,
-        acc,
-        jmp
-    };
-
-    /*
-     * --- Day 8: Handheld Halting ---
-     * Your flight to the major airline hub reaches cruising altitude without incident. While you consider checking the
-     * in-flight menu for one of those drinks that come with a little umbrella, you are interrupted by the kid sitting
-     * next to you.
-     * 
-     * Their handheld game console won't turn on! They ask if you can take a look.
-     * 
-     * You narrow the problem down to a strange infinite loop in the boot code (your puzzle input) of the device. You
-     * should be able to fix it, but first you need to be able to run the code in isolation.
-     * 
-     * The boot code is represented as a text file with one instruction per line of text. Each instruction consists of
-     * an operation (acc, jmp, or nop) and an argument (a signed number like +4 or -20).
-     * 
-     * acc increases or decreases a single global value called the accumulator by the value given in the argument. For
-     *    example, acc +7 would increase the accumulator by 7. The accumulator starts at 0. After an acc instruction,
-     *    the instruction immediately below it is executed next.
-     * jmp jumps to a new instruction relative to itself. The next instruction to execute is found using the argument
-     *    as an offset from the jmp instruction; for example, jmp +2 would skip the next instruction, jmp +1 would
-     *    continue to the instruction immediately below it, and jmp -20 would cause the instruction 20 lines above to
-     *    be executed next.
-     * nop stands for No OPeration - it does nothing. The instruction immediately below it is executed next.
-     * 
-     * For example, consider the following program:
-     * 
-     * nop +0
-     * acc +1
-     * jmp +4
-     * acc +3
-     * jmp -3
-     * acc -99
-     * acc +1
-     * jmp -4
-     * acc +6
-     * 
-     * These instructions are visited in this order:
-     * 
-     * nop +0  | 1
-     * acc +1  | 2, 8(!)
-     * jmp +4  | 3
-     * acc +3  | 6
-     * jmp -3  | 7
-     * acc -99 |
-     * acc +1  | 4
-     * jmp -4  | 5
-     * acc +6  |
-     * 
-     * First, the nop +0 does nothing. Then, the accumulator is increased from 0 to 1 (acc +1) and jmp +4 sets the next
-     * instruction to the other acc +1 near the bottom. After it increases the accumulator from 1 to 2, jmp -4
-     * executes, setting the next instruction to the only acc +3. It sets the accumulator to 5, and jmp -3 causes the
-     * program to continue back at the first acc +1.
-     * 
-     * This is an infinite loop: with this sequence of jumps, the program will run forever. The moment the program
-     * tries to run any instruction a second time, you know it will never terminate.
-     * 
-     * Immediately before the program would run an instruction a second time, the value in the accumulator is 5.
-     * 
-     * Run your copy of the boot code. Immediately before any instruction is executed a second time, what value is in
-     * the accumulator?
-     */
-    static class Part1
-    {
-        public static void Exec()
+        /*
+         * --- Day 8: Handheld Halting ---
+         * Your flight to the major airline hub reaches cruising altitude without incident. While you consider checking the
+         * in-flight menu for one of those drinks that come with a little umbrella, you are interrupted by the kid sitting
+         * next to you.
+         * 
+         * Their handheld game console won't turn on! They ask if you can take a look.
+         * 
+         * You narrow the problem down to a strange infinite loop in the boot code (your puzzle input) of the device. You
+         * should be able to fix it, but first you need to be able to run the code in isolation.
+         * 
+         * The boot code is represented as a text file with one instruction per line of text. Each instruction consists of
+         * an operation (acc, jmp, or nop) and an argument (a signed number like +4 or -20).
+         * 
+         * acc increases or decreases a single global value called the accumulator by the value given in the argument. For
+         *    example, acc +7 would increase the accumulator by 7. The accumulator starts at 0. After an acc instruction,
+         *    the instruction immediately below it is executed next.
+         * jmp jumps to a new instruction relative to itself. The next instruction to execute is found using the argument
+         *    as an offset from the jmp instruction; for example, jmp +2 would skip the next instruction, jmp +1 would
+         *    continue to the instruction immediately below it, and jmp -20 would cause the instruction 20 lines above to
+         *    be executed next.
+         * nop stands for No OPeration - it does nothing. The instruction immediately below it is executed next.
+         * 
+         * For example, consider the following program:
+         * 
+         * nop +0
+         * acc +1
+         * jmp +4
+         * acc +3
+         * jmp -3
+         * acc -99
+         * acc +1
+         * jmp -4
+         * acc +6
+         * 
+         * These instructions are visited in this order:
+         * 
+         * nop +0  | 1
+         * acc +1  | 2, 8(!)
+         * jmp +4  | 3
+         * acc +3  | 6
+         * jmp -3  | 7
+         * acc -99 |
+         * acc +1  | 4
+         * jmp -4  | 5
+         * acc +6  |
+         * 
+         * First, the nop +0 does nothing. Then, the accumulator is increased from 0 to 1 (acc +1) and jmp +4 sets the next
+         * instruction to the other acc +1 near the bottom. After it increases the accumulator from 1 to 2, jmp -4
+         * executes, setting the next instruction to the only acc +3. It sets the accumulator to 5, and jmp -3 causes the
+         * program to continue back at the first acc +1.
+         * 
+         * This is an infinite loop: with this sequence of jumps, the program will run forever. The moment the program
+         * tries to run any instruction a second time, you know it will never terminate.
+         * 
+         * Immediately before the program would run an instruction a second time, the value in the accumulator is 5.
+         * 
+         * Run your copy of the boot code. Immediately before any instruction is executed a second time, what value is in
+         * the accumulator?
+         */
+        static void Part1()
         {
-            if (Run(Data.Test.Item1) != Data.Test.Item2)
-                throw new ApplicationException("Test data was not validated correctly, cannot continue");
+            Computer cmp = null;
+            int result = 0;
 
-            var result = Run(Data.Actual);
-            Console.WriteLine(result);
+            {
+                cmp = new Computer(Data.Test.Item1);
+                bool retval = cmp.Run();
+                result = cmp.State.Eax;
+
+                if (retval)
+                    throw new ApplicationException("Test program did not run into a loop as it was expected to do! Cannot continue.");
+                else if (result != Data.Test.Item2)
+                    throw new ApplicationException("Test data was not executed correctly, cannot continue.");
+
+                cmp = null;
+                result = 0;
+                retval = false;
+            }
+
+            cmp = new Computer(Data.Actual);
+            cmp.Run();
+            Console.WriteLine(cmp.State.Eax);
             // Solution: 1501
         }
 
-        static Tuple<OpCode, int> Parse(in string data)
+        /*
+         * --- Part Two ---
+         * After some careful analysis, you believe that exactly one instruction is corrupted.
+         * 
+         * Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc
+         * instructions were harmed in the corruption of this boot code.)
+         * 
+         * The program is supposed to terminate by attempting to execute an instruction immediately after the last
+         * instruction in the file. By changing exactly one jmp or nop, you can repair the boot code and make it terminate
+         * correctly.
+         * 
+         * For example, consider the same program from above:
+         * 
+         * nop +0
+         * acc +1
+         * jmp +4
+         * acc +3
+         * jmp -3
+         * acc -99
+         * acc +1
+         * jmp -4
+         * acc +6
+         * 
+         * If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop,
+         * never leaving that instruction. If you change almost any of the jmp instructions, the program will still
+         * eventually find another jmp instruction and loop forever.
+         * 
+         * However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The
+         * instructions are visited in this order:
+         * 
+         * nop +0  | 1
+         * acc +1  | 2
+         * jmp +4  | 3
+         * acc +3  |
+         * jmp -3  |
+         * acc -99 |
+         * acc +1  | 4
+         * nop -4  | 5
+         * acc +6  | 6
+         * 
+         * After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last
+         * instruction in the file. With this change, after the program terminates, the accumulator contains the value 8
+         * (acc +1, acc +1, acc +6).
+         * 
+         * Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp). What is the
+         * value of the accumulator after the program terminates?
+         */
+        static void Part2()
         {
-            if (!Enum.TryParse(data[0..3], out OpCode oc))
-                oc = OpCode.invalid;
-
-            return new Tuple<OpCode, int>(oc, int.Parse(data[4..]));
-        }
-
-        static int Run(in List<string> program)
-        {
-            int eax = 0;                // Accumulator
-            int eip = 0;                // Instruction pointer (current index)
-            var esp = new List<int>();  // Stack pointer (visited indices)
-
-            while (!esp.Contains(eip) && eip < program.Count)
+            List<Instruction> program = null;
+            int result = 0;
             {
-                esp.Add(eip);
+                program = Instruction.Parse(Data.Test.Item1);
+                result = Part2Solver(program);
+                if (result != Data.Test.Item3)
+                    throw new ApplicationException("Test program failed to complete.");
 
-                var instruction = Parse(program[eip]);
-                switch (instruction.Item1)
-                {
-                    case OpCode.nop:
-                        break;
-                    case OpCode.acc:
-                        eax += instruction.Item2;
-                        break;
-                    case OpCode.jmp:
-                        eip += instruction.Item2;
-                        continue;
-                    case OpCode.invalid:
-                    default:
-                        throw new ApplicationException($"Unknown opcode: '{program[eip]}'");
-                }
-                eip++;
+                program.Clear();
+                result = 0;
             }
 
-            return eax;
+            program = Instruction.Parse(Data.Actual);
+            result = Part2Solver(program);
+            Console.Write(result);
+            // Solution: 509
+        }
+
+        static int Part2Solver(List<Instruction> instructions)
+        {
+            var cmp = new Computer(instructions);
+            var failedMods = new List<int>((int)(0.66f * instructions.Count));
+            int lastMod = -1;
+
+            while (!cmp.Run())
+            {
+                if (lastMod >= 0)
+                {
+                    cmp.Program[lastMod].Flip();
+                    failedMods.Add(lastMod);
+                }
+
+                // TODO: linq over cmp.state.esp wasn't working for some reason.
+                // Also, this doesn't check for an unsolvable problem. oops.
+                var stack = new List<int>(cmp.State.Esp);
+                stack.Reverse();
+
+                foreach(var idx in stack)
+                {
+                    if (cmp.Program[idx].CanFlip && !failedMods.Contains(idx))
+                    {
+                        cmp.Program[idx].Flip();
+                        lastMod = idx;
+                        break;
+                    }
+                }
+
+                cmp.State.Reset();
+            }
+
+            return cmp.State.Eax;
         }
     }
 
     static class Data
     {
+        // Tuple<Program, Accumulator-pt1, Accumulator-pt2>
         public static readonly Tuple<List<string>, int, int> Test = new Tuple<List<string>, int, int>(
         new List<string> {
-            "nop +0",
-            "acc +1",
-            "jmp +4",
-            "acc +3",
-            "jmp -3",
-            "acc -99",
-            "acc +1",
-            "jmp -4",
-            "acc +6"
-        }, 5, 0);
+            "nop +0", // 0: 0
+            "acc +1", // 1: 1; **7**
+            "jmp +4", // 2: 2
+            "acc +3", // 3: 5
+            "jmp -3", // 4: 6
+            "acc -99",// 5:
+            "acc +1", // 6: 3
+            "jmp -4", // 7: 4; **nop -4**
+            "acc +6"  // 8:
+        }, 5, 8);
 
         public static readonly List<string> Actual = new List<string>
         {
